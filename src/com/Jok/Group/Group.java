50b89e8f79de5849345d097b1f46980532a7bece
JokHornsby <jokhornsby@gmail.com>
2014-10-08 21:39:39 +0100

    New commit

--- src/com/Jok/Group/Group.java
+++ src/com/Jok/Group/Group.java
@@ -1,14 +1,27 @@
 package com.Jok.Group;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bukkit.entity.Player;
 
 public class Group {
 	
-	String name = "";
+	private String name = "";
 	
-	List<Player> players = new ArrayList<Player>();
+	public Group(String name) {
+		this.name = name;
+	}
+	
+	public void addPlayer(Player player) {
+		if(!Main.players.containsValue(this)) {
+			Main.players.put(player.getUniqueId(), this);
+
+			player.sendMessage("§7[§cJokGroup§7] You are now in group '" + name + "'.");
+		}
+		else
+			player.sendMessage("§7[§cJokGroup§7] §cYou already belong to this group.");
+	}
+	
+	public String getName() {
+		return name;
+	}
 	
 }
--- src/com/Jok/Group/Main.java
+++ src/com/Jok/Group/Main.java
@@ -1,7 +1,9 @@
 package com.Jok.Group;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -10,7 +12,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	List<String> groups = new ArrayList<String>();
+	public static List<Group> groups = new ArrayList<Group>();
+	public static HashMap<UUID, Group> players = new HashMap<UUID, Group>();
 	
 	@Override
     public void onEnable() {
@@ -22,25 +25,60 @@ public class Main extends JavaPlugin {
 		
 	}
 	
+	/**
+	 * Returns the group with the specified name, or null.
+	 */
+	public Group getGroup(String groupName) {
+		for (int i = 0; i < groups.size(); i++) {
+			if(groups.get(i).getName().equals(groupName))
+				return groups.get(i);
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Returns the group that belongs to that player, or null.
+	 */ 
+	public Group getGroup(Player player) {
+		return players.get(player.getUniqueId());
+	}
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player)sender;
+		
 		if (cmd.getName().equalsIgnoreCase("j")) {
 			if(args[0].equalsIgnoreCase("create")) {
+				Group group;
 				String groupName = args[1];
 				
-				if(!groups.contains(groupName))
-					groups.add(groupName);
-				else {
-					print((Player)sender, "§cGroup '" + groupName + "' already exists.");
+				if(getGroup(groupName) != null) {
+					print(player, "§cGroup '" + groupName + "' already exists.");
 					return true;
+				} else {
+					group = new Group(groupName);
+					groups.add(group);
 				}
 				
-				print((Player)sender, "Created group '" + args[1] + "'.");
+				print(player, "Created group '" + groupName + "'.");
+				group.addPlayer(player);
+				
+				return true;
+			} else if(args[0].equalsIgnoreCase("join")) {
+				Group group = getGroup(args[1]);
+				
+				if(group == null) {
+					print(player, "§cGroup '" + args[1] + "' doesn't exist.");
+					return true;
+				} else {
+					group.addPlayer(player);
+				}
 			} else if(args[0].equalsIgnoreCase("list")) {
-				print((Player)sender, "All Groups:");
+				print(player, "All Groups:");
 				
 				for (int i = 0; i < groups.size(); i++) {
-					print((Player)sender, " - §9" + groups.get(i));
+					print(player, " - §9" + groups.get(i).getName());
 				}
 			}
 			
